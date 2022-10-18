e2faff890407bf782b2c35506efaea105757907f
jorgegoco <jorgegoco70@gmail.com>
2022-10-18 19:00:12 +0200

    Add mobile menu popup window

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -75,3 +75,16 @@ for(let i = 0; i < speakers.length; i += 1) {
   speaker.appendChild(speakerData);
   speakersAll.appendChild(speaker);
 }
+
+function showMenu() {
+  document.querySelector('.main-page').style.display = 'none';
+  document.querySelector('.mobile-popup-menu').style.display = 'block';
+}
+
+function hideMenu() {
+  document.querySelector('.main-page').style.display = 'block';
+  document.querySelector('.mobile-popup-menu').style.display = 'none';
+}
+
+document.querySelector('.fa-bars').addEventListener('click', showMenu);
+document.querySelector('#close-button').addEventListener('click', hideMenu);
